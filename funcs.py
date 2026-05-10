3632d36a9402dc12db19af6529e47231ff26659e
Mard <mardjavid1@gmail.com>
2026-05-10 19:43:12 +0400

    Replace main.py with funcs.py

---
+++ funcs.py
@@ -0,0 +1,79 @@
+# =============================== #
+#            FUNCTIONS            #
+# =============================== #
+
+# ============ UNITS ============ #
+
+# Mass:                   kg
+# Pressure:               kPa
+# Temperature:            K
+# Volume:                 m ^ 3
+# Work:                   kJ
+# Specific volume:        (m ^ 3) / kg
+# Specific heat capacity: kJ / (kg * K)
+# Gas constant:           kJ / (kg * K)
+
+# =============================== #
+
+
+# Returns an array containing 4 tuples of (P, v, T) in (kPa, (m ^ 3) / kg)
+# Input:
+# P1    - Compressor inlet pressure          (kPa)
+# T1    - Compressor inlet temperature       (K)
+# T3    - Maximum cycle temperature          (K)
+# rp    - Pressure ratio                     (dimensionless)
+# cp    - Specific heat at constant pressure (kJ / (kg * K))
+# gamma - heat capacity ratio                (dimensionless)
+def getStateVariables(P1, T1, T3, rp, cp, gamma):
+
+    P2 = P1 * rp                            # Compressor outlet pressure                (kPa)
+    P3 = P2                                 # Turbine inlet pressure                    (kPa)
+    P4 = P1                                 # Turbine outlet pressure                   (kPa)
+        
+    T2 = T1 * (rp ** ((gamma - 1) / gamma)) # Compressor outlet temperature             (K)
+    T4 = T3 / (rp ** ((gamma - 1) / gamma)) # Turbine outlet temperature                (K)
+    
+    R = ((gamma - 1) * cp) / gamma          # Gas constant                              (kJ / (kg * K))
+    v1 = R * T1 / P1                        # Compressor inlet specific volume          ((m ^ 3) / kg)
+    v2 = R * T2 / P2                        # Compressor outlet specific volume         ((m ^ 3) / kg)
+    v3 = R * T3 / P3                        # Turbine inlet specific volume             ((m ^ 3) / kg)
+    v4 = R * T4 / P4                        # Turbine outlet specific volume            ((m ^ 3) / kg)
+
+    return [
+        (P1, v1, T1),
+        (P2, v2, T2),
+        (P3, v3, T3),
+        (P4, v4, T4),
+    ]
+
+# Returns a tuple containing Compressor work, turbine work and net work in order
+# Input:
+# m  - Mass                               (kg)
+# cp - Specific heat at constant pressure (kJ / (kg * K))
+# T1 - Compressor inlet temperature       (K)
+# T2 - Compressor outlet temperature      (K)
+# T3 - Maximum cycle temperature          (K)
+# T4 - Turbine outlet temperature         (K)
+def getWork(m, cp, T1, T2, T3, T4):    
+    Wc   = m * cp * (T2 - T1)               # Compressor work                           (kJ)
+    Wt   = m * cp * (T3 - T4)               # Turbine work                              (kJ)
+    Wnet = Wt - Wc                          # Net work                                  (kJ)
+    return (Wc, Wt, Wnet)
+
+# Returns a tuple containing heat added, heat rejected, thermal efficiency and back work ratio in order
+# Input:
+# m    - Mass                               (kg)
+# cp   - Specific heat at constant pressure (kJ / (kg * K))
+# T1   - Compressor inlet temperature       (K)
+# T2   - Compressor outlet temperature      (K)
+# T3   - Maximum cycle temperature          (K)
+# T4   - Turbine outlet temperature         (K)
+# Wc   - Compressor work                    (kJ)
+# Wt   - Turbine work                       (kJ)
+# Wnet - Net work                           (kJ)
+def getHeat(m, cp, T1, T2, T3, T4, Wc, Wt, Wnet):
+    Qin  = m * cp * (T3 - T2)               # Heat added                                (kJ)
+    Qout = m * cp * (T4 - T1)               # Heat rejected                             (kJ)
+    eta  = Wnet / Qin                       # Thermal efficiency                        (dimensionless)
+    bwr  = Wc / Wt                          # Back Work Ratio                           (dimensionless)
+    return (Qin, Qout, eta, bwr)
